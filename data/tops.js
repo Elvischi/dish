c4bec4c44d7a796ef0b9ee5ee51bb79b8c37ef92
Matic Elvis <elvismatic21@gmail.com>
2025-09-10 08:12:33 -0700

    module

---
+++ data/tops.js
@@ -0,0 +1,44 @@
+
+export const products = [
+  {
+    image: "./image/skull.jpeg",
+    name: "Skull Tee",
+    price: 5000,
+  },
+  {
+    image: "./image/omw.jpg",
+    name: "Regular Fit Shortsleeve",
+    price: 5000,
+  },
+  {
+    image: "./image/under black.jpeg",
+    name: "Underworld Tees",
+    price: 5000,
+  },
+  {
+    image: "./image/omw update black.jpeg",
+    name: "Graphics Tees",
+    price: 5000,
+  },
+  {
+    image: "./image/armless.jpg",
+    name: "Armless Piece",
+    price: 5000,
+  },
+  {
+    image: "./image/shortsleeves.jpg",
+    name: "Brand T-Shirt Shortsleeves",
+    price: 5000,
+  },
+  {
+    image: "./image/smoke black.jpeg",
+    name: "Deep Thoughts Tees",
+    price: 5000,
+  },
+  {
+    image: "./image/wolf tee.jpg",
+    name: "Wolf Tee",
+    price: 5000
+  }
+];
+
